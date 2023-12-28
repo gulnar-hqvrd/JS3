89a616529f0578f057fe6709b71867ee1ef06d42
Gulnar Hagverdiyeva <glnrhagverdiyeva@gmail.com>
2023-12-28 18:30:26 +0400

    first commit

---
+++ script/example.js
@@ -0,0 +1,92 @@
+// -------------------------------------------
+// let n = +prompt("Ededi daxil edin");
+// let s = "";
+// for (let i = 1; i <= n; i++) {
+//  s += i
+//  console.log(s);
+// }
+
+// --------------------------------------------------
+// let a = +prompt("ededi daxil edin")
+// let b = +prompt("ededi daxil edin")
+
+// for(a;a<=b ;a++){
+//     if(a % 2 ==0){
+//         console.log(a);
+//     }
+// }
+
+// ------------------------------------------------------
+
+// let a = Number(prompt("Reqemi yaz"));
+// let say = 0;
+
+// for ( let bolen = 1; bolen <= a ; bolen++)
+//  {
+//     if (a % bolen === 0 ){ 
+//     say = say + 1  
+//  }
+// }
+// if (say > 2){
+//     console.log("Murekkeb");
+// }
+
+// else {
+//     console.log("Sade");
+// }
+
+
+// let isPrime = true;
+// let value = 17;
+// for (let i = 2 ; i < value/2 ; i++){
+//     // console.log(i);
+//     if(value % i === 0){
+//         isPrime = false;
+//         break;
+//     }
+
+// }
+
+// console.log(isPrime ? "Sade Ededdir": "Murekkebdir");
+
+// -----------------------------------------
+// let a = 2
+// let b = 12;
+
+// for( i = a ; i <=b ; a++){
+//     let isPrime = true;
+// for (let i = 2 ; i < value/2 ; i++){
+//     // console.log(i);
+//     if(value % i === 0){
+//         isPrime = false;
+//         break;
+//     }
+
+// }
+// if(isPrime){
+//     console.log(i);
+// }
+
+// }
+
+
+
+// let e = +prompt("Ededi yaz");
+// let s = 0;
+// while (e) {
+//   s += e % 10;
+//   e = parseInt(e / 10);
+// //   console.log(e);
+// }
+// console.log(s);
+
+
+
+
+// let a = +prompt("Ededi daxil edin")
+// let fac = 1
+// for (b = 1 ; b <=a ; b++){
+//     fac = fac * b
+   
+// }
+// console.log(fac);
